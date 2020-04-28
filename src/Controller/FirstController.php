119bdfc56afd27205232f3abc221b15ad1215ca0
Mohamed <mohamedbensalah@insat.u-carthage.tn>
2020-04-28 14:26:47 +0200

    N/A

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use http\Env\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    /**
+     * @Route("/first", name="first")
+     */
+    public function index()
+    {
+        return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    /**
+     * @Route("/test")
+     */
+    public function tsestingReq(\Symfony\Component\HttpFoundation\Request $request){
+        dd($request);
+    }
+}
